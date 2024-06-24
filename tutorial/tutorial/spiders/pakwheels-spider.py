408cb1a3737c145a11932d8c04d8442d8a8f38a8
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-06-24 19:12:11 +0500

    Updated Pakwheels scraper with implemented rule for next page. saved data in json files. bypassed site blockage using headers

--- tutorial/tutorial/spiders/pakwheels-spider.py
+++ tutorial/tutorial/spiders/pakwheels-spider.py
@@ -1,93 +1,106 @@
-import scrapy
+import json
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 
-class UsedCarsSpider(scrapy.Spider):
-    name = "pakwheels"  
+class PakwheelsUsedCarsSpider(CrawlSpider):
+    name = "pakwheels"
+    ad_descriptions = []
+    start_urls = [
+        "https://www.pakwheels.com/used-cars/search/-/"
+    ]
 
-    def start_requests(self):
-        urls = [
-            "https://www.pakwheels.com/used-cars/search/-/"
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
+    rules = (
+         Rule(LinkExtractor(allow=r"https://www.pakwheels.com/used-cars/search/-/\?page=\d+"), callback="parse", follow=True),
+    )
 
     def parse(self, response):
-
-        listings = response.css(".classified-listing")
-        for listing in listings:
+        for listing in response.css(".classified-listing"):
             link = listing.css('a.car-name::attr(href)').get()
-            yield response.follow(link, callback=self.parse_description)
-
-        next_page = response.css('ul.pagination.search-pagi li.next_page a::attr(href)').get()
-        if next_page:
-            print("page ******************************************************************************************************* : ",next_page)
-            yield response.follow(next_page, callback=self.parse)
-
-
+            yield response.follow(link, callback=self.parse_vehicle_description)
+    
     def get_ad_title(self, response):
         title = response.css(".well h1::text").get()
-        print(title)
+        return title
 
     def get_vehicle_price(self, response):
         price = response.css("strong.generic-green::text").get()
-        print(price)
+        return price
 
     def get_model_year(self, response):
         year = response.css(".table tr:nth-child(1) td:nth-child(1) a::text").get()
-        print(year)
+        return year
 
     def get_vehicle_millage(self, response):
         millage = response.css(".table tr:nth-child(1) td:nth-child(2) p::text").get()
-        print(millage)
+        return millage
 
     def get_vehicle_fuel_type(self, response):
         fuel_type = response.css(".table tr:nth-child(1) td:nth-child(3) a::text").get()
-        print(fuel_type)
+        return fuel_type
 
     def get_vehicle_transmission_type(self, response):
         transmission = response.css(".table tr:nth-child(1) td:nth-child(4) a::text").get()
-        print(transmission)
+        return transmission
 
     def get_vehicle_features(self, response):
+        features = []
         for li in response.css('ul.list-unstyled.car-feature-list.nomargin li'):
-            feature = li.css('::text').get().strip()
-            print(feature,end=",")
+            features.append(li.css('::text').get().strip())
+        return features 
 
     def get_seller_comments(self, response):
         seller_comments = response.css('h2#scroll_seller_comments+ div::text').get()
-        print(f"\n{seller_comments.strip()}")
+        return seller_comments
 
     def get_seller_name(self, response):
-        seller_name = response.css("h5.nomargin::text").get()
-        print(seller_name)
+        seller_name = response.css(".col-md-9 h5.nomargin::text").get()
+        if not seller_name:
+            seller_name = response.css(".col-md-9 label[itemprop='name'] a::text").get()
+        return seller_name
 
     def get_image_src(self, response):
         image_src = response.css(".gallery li img::attr(src)").getall()
-        print(image_src)
+        return image_src
         
-
-
-    def get_vehicle_description(self, response):
-        description_li_elements = response.css('ul.list-unstyled.ul-featured.clearfix li')
+    def get_vehicle_information(self, response):
+        key_text = []
+        value_text = []
         description_dict = {}
-        for li in range(0, len(description_li_elements)-1, 2):
-            key_text = description_li_elements[li].css('::text').get().strip()
-            value_text = description_li_elements[li + 1].css('::text').get().strip()
-            description_dict[key_text] = value_text
-        print(description_dict)
 
+        for list_item in response.css("li.ad-data"):    
+            key_text.append(list_item.css('::text').get().strip())
 
-    def parse_description(self, response):
+        for list_item in response.css("ul.list-unstyled.ul-featured.clearfix li:not(.ad-data)"):    
+            value_text.append(list_item.css('::text').get().strip())
+
+        for i in range(len(key_text)):
+            description_dict[key_text[i]] = value_text[i]
+        return description_dict
+
+    def parse_vehicle_description(self, response):
         
-        self.get_ad_title(response)
-        self.get_vehicle_price(response)
-        self.get_model_year(response)
-        self.get_vehicle_millage(response)
-        self.get_vehicle_fuel_type(response)
-        self.get_vehicle_transmission_type(response)
-        self.get_vehicle_features(response)
-        self.get_seller_comments(response)
-        self.get_seller_name(response)
-        self.get_image_src(response)
-        self.get_vehicle_description(response)
+        vehicle_ad_description = {
+            'vehicle_ad_title' : self.get_ad_title(response),
+            'vehicle_price' : self.get_vehicle_price(response),
+            'vehicle_model_year' : self.get_model_year(response),
+            'vehicle_millage' : self.get_vehicle_millage(response),
+            'vehicle_fuel_type' : self.get_vehicle_fuel_type(response),
+            'vehicle_transmission_type' : self.get_vehicle_transmission_type(response),
+            'seller_comments' : self.get_seller_comments(response),
+            'seller_name' : self.get_seller_name(response),
+            'vehicle_information' : self.get_vehicle_information(response),
+            'vehicle_features' : self.get_vehicle_features(response),
+            'vehicle_images_src' : self.get_image_src(response)
+
+        }      
+        self.ad_descriptions.append(vehicle_ad_description)
+
+        
+    def write_ad_description_to_json(self, ad_description):
+        with open("Pakwheels_Used_Cars_ad.json","w") as pakwheels_json:
+            json.dump(ad_description, pakwheels_json, indent=4)
+
+    def close(self, Spider, reason: str):
+        self.write_ad_description_to_json()
+        super().close(reason)
