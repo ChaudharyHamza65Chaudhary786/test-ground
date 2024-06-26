67028abebce07e908b7ec95a4e323f714d360999
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-06-27 00:31:51 +0500

    Removed extra varibles used list comprehensions eliminated manual file handling pep-8 conventions

--- tutorial/tutorial/spiders/pakwheels-spider.py
+++ tutorial/tutorial/spiders/pakwheels-spider.py
@@ -1,4 +1,3 @@
-import json
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -12,10 +11,14 @@ class PakwheelsUsedCarsSpider(CrawlSpider):
     ]
 
     rules = (
-         Rule(LinkExtractor(allow=r"https://www.pakwheels.com/used-cars/search/-/\?page=\d+", restrict_css="li.next_page a"),  
-              callback="parse", 
-              follow=True
-            ),
+        Rule(
+            LinkExtractor(
+                allow=r"https://www.pakwheels.com/used-cars/search/-/\?page=\d+",
+                restrict_css="li.next_page a"
+            ),  
+            callback="parse", 
+            follow=True
+        ),
     )
 
     def parse(self, response):
@@ -24,38 +27,28 @@ class PakwheelsUsedCarsSpider(CrawlSpider):
             yield response.follow(link, callback=self.parse_vehicle_description)
     
     def get_ad_title(self, response):
-        title = response.css(".well h1::text").get()
-        return title
+        return response.css(".well h1::text").get()
 
-    def get_vehicle_price(self, response):
-        price = response.css("strong.generic-green::text").get()    
-        return price
+    def get_vehicle_price(self, response):    
+        return response.css("strong.generic-green::text").get() 
 
     def get_model_year(self, response):
-        year = response.css(".table tr:nth-child(1) td:nth-child(1) a::text").get()
-        return year
+        return response.css(".table tr:nth-child(1) td:nth-child(1) a::text").get()
 
     def get_vehicle_millage(self, response):
-        millage = response.css(".table tr:nth-child(1) td:nth-child(2) p::text").get()
-        return millage
+        return response.css(".table tr:nth-child(1) td:nth-child(2) p::text").get()
 
     def get_vehicle_fuel_type(self, response):
-        fuel_type = response.css(".table tr:nth-child(1) td:nth-child(3) a::text").get()
-        return fuel_type
+        return response.css(".table tr:nth-child(1) td:nth-child(3) a::text").get()
 
     def get_vehicle_transmission_type(self, response):
-        transmission = response.css(".table tr:nth-child(1) td:nth-child(4) a::text").get()
-        return transmission
-
-    def get_vehicle_features(self, response):
-        features = []
-        for li in response.css('ul.list-unstyled.car-feature-list.nomargin li'):
-            features.append(li.css('::text').get().strip())
-        return features 
+        return response.css(".table tr:nth-child(1) td:nth-child(4) a::text").get()
+    
+    def get_image_src(self, response):
+        return response.css(".gallery li img::attr(src)").getall()
 
     def get_seller_comments(self, response):
-        seller_comments = response.css('h2#scroll_seller_comments+ div::text').get()
-        return seller_comments
+        return response.css('h2#scroll_seller_comments+ div::text').get()
 
     def get_seller_name(self, response):
         seller_name = response.css(".col-md-9 h5.nomargin::text").get()
@@ -63,28 +56,20 @@ class PakwheelsUsedCarsSpider(CrawlSpider):
             seller_name = response.css(".col-md-9 label[itemprop='name'] a::text").get()
         return seller_name
 
-    def get_image_src(self, response):
-        image_src = response.css(".gallery li img::attr(src)").getall()
-        return image_src
-        
+    def get_vehicle_features(self, response):
+        return [li.css('::text').get().strip() for li in response.css('ul.list-unstyled.car-feature-list.nomargin li')]
+    
     def get_vehicle_information(self, response):
-        key_text = []
-        value_text = []
         description_dict = {}
+        key_text = [list_item.css('::text').get().strip() for list_item in response.css("li.ad-data")]
+        value_text = [list_item.css('::text').get().strip() for list_item in response.css("ul.list-unstyled.ul-featured.clearfix li:not(.ad-data)")]
 
-        for list_item in response.css("li.ad-data"):    
-            key_text.append(list_item.css('::text').get().strip())
-
-        for list_item in response.css("ul.list-unstyled.ul-featured.clearfix li:not(.ad-data)"):    
-            value_text.append(list_item.css('::text').get().strip())
-
-        for i in range(len(key_text)):
-            description_dict[key_text[i]] = value_text[i]
+        for index in range(len(key_text)):
+            description_dict[key_text[index]] = value_text[index]
         return description_dict
 
     def parse_vehicle_description(self, response):
-        
-        vehicle_ad_description = {
+        yield  {
             'vehicle_ad_title' : self.get_ad_title(response),
             'vehicle_price' : self.get_vehicle_price(response),
             'vehicle_model_year' : self.get_model_year(response),
@@ -97,12 +82,4 @@ class PakwheelsUsedCarsSpider(CrawlSpider):
             'vehicle_features' : self.get_vehicle_features(response),
             'vehicle_images_src' : self.get_image_src(response)
         }      
-        self.ad_descriptions.append(vehicle_ad_description)
-    
-    def write_ad_description_to_json(self):
-        with open("Pakwheels_Used_Cars_ad.json","w") as pakwheels_json:
-            json.dump(self.ad_descriptions, pakwheels_json, indent=4)
-
-    def close(self, reason: str):
-        self.write_ad_description_to_json()
-        super().close(reason)
+ 
