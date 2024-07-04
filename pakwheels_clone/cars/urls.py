c654f7d9cf9655057744858a59ac1b772d3c79ea
ChaudharyHamza65Chaudhary786 <hamzakhalidpk65@gmail.com>
2024-07-04 12:54:09 +0500

    removed all

--- pakwheels_clone/cars/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
-from .views import CarsListView
-
-urlpatterns = [
-    path("", CarsListView.as_view(), name="cars_list"),
-]
